6225573c5803609f641e7d29c48ccaa4910a1c9f
Omar Zekri <102541219+Omar50x@users.noreply.github.com>
2022-10-21 14:58:44 +0100

    Add Footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,20 @@ import './contact.css'
 import {MdOutlineMail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_b00kojs', 'template_4vfj4va', form.current, 'Lz5vAAid0E-nhKKeZ');
+
+    e.target.reset();
+  };
+  
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -12,26 +24,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMail />
+            <MdOutlineMail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>omarzekri217@gmail.com</h5>
-            <a href="mailto:omarzekri217@gmail.com">Send a message</a>
+            <a href="mailto:omarzekri217@gmail.com" target="blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>OmarZekri</h5>
-            <a href="https://www.facebook.com/omar.zakri.56/">Send a message</a>
+            <a href="https://www.facebook.com/omar.zakri.56/" target="blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+212 0643076263</h5>
-            <a href="https://api.whatsapp.com/send?phone">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone+2120643076263" target="blank">Send a message</a>
           </article>
         </div>
         {/* End Of Contact Options */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Full Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
