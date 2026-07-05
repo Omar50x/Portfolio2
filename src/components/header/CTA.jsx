8b09570bb6971b5a7e6e7ac14a1b31dbfaa92b50
Omar50x <omarzekri217@gmail.com>
2026-07-05 18:56:44 +0100

    change cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={`${process.env.PUBLIC_URL}/cv.pdf`} download='Omar-Zekri-CV.pdf' className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
